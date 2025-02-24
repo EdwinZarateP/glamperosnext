bbf71b475d761d5620081bef44fd1c8e92669d6b
EdwinZarateP <emzp1994@gmail.com>
2025-02-23 22:12:56 -0500

    se arregal lotie

--- src/app/Reservar/page.tsx
+++ src/app/Reservar/page.tsx
@@ -1,11 +1,30 @@
 "use client";
-export const dynamic = "force-dynamic";
 import { useEffect, useState } from 'react';
-import Lottie from 'lottie-react';
+import dynamic from "next/dynamic";
 import animationData from "@/Componentes/Animaciones/AnimationPuntos.json";
 import Reservacion from '@/Componentes/Reservacion/index';
 import HeaderIcono from '@/Componentes/HeaderIcono/index';
 import './estilos.css';
+
+
+interface MyLottieProps {
+  animationData: unknown;
+  loop?: boolean;
+  autoplay?: boolean;
+  style?: React.CSSProperties;
+}
+
+// Transformamos la importación de `lottie-react` a un componente que acepte MyLottieProps
+const Lottie = dynamic<MyLottieProps>(
+  () =>
+    import("lottie-react").then((mod) => {
+      // forzamos el default a un componente tipado
+      return mod.default as React.ComponentType<MyLottieProps>;
+    }),
+  {
+    ssr: false,
+  }
+);
 
 function Reservar() {
   // Estado para mostrar/ocultar la animación de carga
@@ -28,11 +47,11 @@ function Reservar() {
 
       {isLoading && (
         <div className="lottie-container">
-          <Lottie 
+          <Lottie
             animationData={animationData}
-            style={{ width: 300, height: 300 }}
-            loop
-            autoplay
+            loop={true}
+            autoplay={true}
+            style={{ height: 200, width: "100%", margin: "auto" }}
           />
         </div>
       )}

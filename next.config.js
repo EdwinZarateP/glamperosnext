c07a74e7fb601e71765b9a8b39971f6d80fb2d48
EdwinZarateP <emzp1994@gmail.com>
2025-02-27 09:27:07 -0500

    se devuelve

--- next.config.js
+++ next.config.js
@@ -9,26 +9,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true, // Opcional: habilita el modo estricto de React
-
-  async redirects() {
-    return [
-      {
-        source: "/Medellin",
-        destination: "/medellin",
-        permanent: true, // 301 - Redirección permanente
-      },
-      {
-        source: "/Bogota",
-        destination: "/bogota",
-        permanent: true, // 301 - Redirección permanente
-      },
-      {
-        source: "/Cali",
-        destination: "/cali",
-        permanent: true, // 301 - Redirección permanente
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;

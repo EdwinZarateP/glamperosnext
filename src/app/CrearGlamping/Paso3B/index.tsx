0499f5b45cb2a088815d970c2cda889d7fd2a09b
EdwinZarateP <emzp1994@gmail.com>
2025-04-09 12:48:20 -0500

    dias cancelacion

--- src/app/CrearGlamping/Paso3B/index.tsx
+++ src/app/CrearGlamping/Paso3B/index.tsx
@@ -65,7 +65,7 @@ const Paso3B: React.FC = () => {
     }
   };
   const handleIncreaseDiasCancelacion = () => {
-    if (diasCancelacion < 10) {
+    if (diasCancelacion < 15) {
       setDiasCancelacion(diasCancelacion + 1);
     }
   };

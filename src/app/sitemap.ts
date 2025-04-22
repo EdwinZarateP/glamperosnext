e24abc6cc9a03a391247dfd4ec08dbe3d53aadcc
EdwinZarateP <emzp1994@gmail.com>
2025-04-22 12:31:19 -0500

    propietarios

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,6 +8,7 @@ export default function sitemap() {
     { url: `${baseUrl}/cali`, lastModified: new Date().toISOString() },
     { url: `${baseUrl}/glampings`, lastModified: new Date().toISOString() },  
     { url: `${baseUrl}/registro`, lastModified: new Date().toISOString() },  
+    { url: `${baseUrl}/propietarios`, lastModified: new Date().toISOString() },  
   ];
 }
 

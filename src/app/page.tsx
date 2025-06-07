8f121555b99ac5ea9bdca0de00af1e043232d4a4
EdwinZarateP <emzp1994@gmail.com>
2025-06-07 18:27:49 -0500

    ajustes sitemap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,26 @@
-bloc
+
+"use client";
+
+import React from "react";
+import TarjetasEcommerce from "@/Componentes/TarjetasEcommerce";
+import MenuUsuariosInferior from "@/Componentes/MenuUsuariosInferior";
+import Footer from "@/Componentes/Footer";
+import "./page.css"; 
+
+export default function GlampingsPage() {
+  return (
+    <>
+      <div className="GlampingsPage-container">
+        <div className="GlampingsPage-tarjetas">
+          <TarjetasEcommerce key="all" />
+        </div>
+        <div className="GlampingsPage-Footer">
+          <Footer />
+        </div>
+        <div className="GlampingsPage-menu">
+          <MenuUsuariosInferior />
+        </div>
+      </div>
+    </>
+  );
+}

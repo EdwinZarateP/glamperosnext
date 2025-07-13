b5564d274768ac3a1a7fdfc2d5eba6040b41f730
Edwin Zarate <emzp1994@gmail.com>
2025-07-13 16:50:52 -0500

    ajustes de precio

--- src/Funciones/ExtraerTarifaGlamperos.tsx
+++ src/Funciones/ExtraerTarifaGlamperos.tsx
@@ -1,12 +1,12 @@
 // Función que calcula el precio con recargo según el precio base
 export const ExtraerTarifaGlamperos = (precio: number): number => {
   if (precio <= 0) return 0;
-  if (precio < 300000) return 0.16;
+  if (precio < 300000) return 0.18;
   if (precio >= 300000 && precio < 400000) return  0.15;
-  if (precio >= 400000 && precio < 500000) return 0.13;
-  if (precio >= 500000 && precio < 600000) return 0.12;
-  if (precio >= 600000 && precio < 800000) return 0.1;
-  if (precio >= 800000 && precio < 2000000) return 0.09;
+  if (precio >= 400000 && precio < 500000) return 0.14;
+  if (precio >= 500000 && precio < 600000) return 0.13;
+  if (precio >= 600000 && precio < 800000) return 0.11;
+  if (precio >= 800000 && precio < 2000000) return 0.1;
   
   return precio;
 };
--- src/Funciones/precioConRecargo.tsx
+++ src/Funciones/precioConRecargo.tsx
@@ -2,12 +2,12 @@
 
 export const precioConRecargo = (precio: number): number => {
   if (precio <= 0) return 0;
-  if (precio <= 299999) return precio * 1.16;
+  if (precio <= 299999) return precio * 1.18;
   if (precio >= 300000 && precio < 400000) return precio * 1.15;
-  if (precio >= 400000 && precio < 500000) return precio * 1.13;
-  if (precio >= 500000 && precio < 600000) return precio * 1.12;
-  if (precio >= 600000 && precio < 800000) return precio * 1.1;
-  if (precio >= 800000 && precio < 2000000) return precio * 1.09;
+  if (precio >= 400000 && precio < 500000) return precio * 1.14;
+  if (precio >= 500000 && precio < 600000) return precio * 1.13;
+  if (precio >= 600000 && precio < 800000) return precio * 1.11;
+  if (precio >= 800000 && precio < 2000000) return precio * 1.1;
 
   return precio;
 };

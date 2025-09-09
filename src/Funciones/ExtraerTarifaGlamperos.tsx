908aa69df9d8de49bababa069db6ec3823974041
Edwin Zarate <emzp1994@gmail.com>
2025-09-08 21:57:12 -0500

    se ajusta tarifas

--- src/Funciones/ExtraerTarifaGlamperos.tsx
+++ src/Funciones/ExtraerTarifaGlamperos.tsx
@@ -2,7 +2,7 @@
 export const ExtraerTarifaGlamperos = (precio: number): number => {
   if (precio <= 0) return 0;
   if (precio < 300000) return 0.2;
-  if (precio >= 300000 && precio < 400000) return  0.15;
+  if (precio >= 300000 && precio < 400000) return  0.16;
   if (precio >= 400000 && precio < 500000) return 0.14;
   if (precio >= 500000 && precio < 600000) return 0.13;
   if (precio >= 600000 && precio < 800000) return 0.11;

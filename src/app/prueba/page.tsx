9d9e7e7f2d72c05c33bb30546cab98d2fa9260d9
Luis Velasquez <luise.velasquezg@gmail.com>
2026-03-27 17:40:25 -0500

    refactor: :recycle: implementa componente visualizador de versiones de diseño

--- src/app/prueba/page.tsx
+++ src/app/prueba/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import ListadoGlampings from "@/Componentes/ListadoGlampingsPrueba/ListadoGlampingsV2";
+import { VersionProvider, useVersion } from "@/context/VersionContext";
+import SectionWrapper from "@/Componentes/Versionamiento/SectionWrapper";
+import ListadoGlampings from "@/Componentes/ListadoGlampingsPrueba/ListadoGlampings";
+import ListadoGlampingsV2 from "@/Componentes/ListadoGlampingsPrueba/ListadoGlampingsV2";
 import HeaderFinal from "../../Componentes/HeaderModificado";
 import "./estilos.css";
 
+function ListadoSection() {
+  const { current } = useVersion("listado", 2);
+  const versiones = [<ListadoGlampings key="v1" />, <ListadoGlampingsV2 key="v2" />];
+  return versiones[current];
+}
+
 function Prueba() {
   return (
-    <div className="prueba-contenedor">
-      <HeaderFinal />
-      <ListadoGlampings />
-    </div>
+    <VersionProvider>
+      <div className="prueba-contenedor">
+        <HeaderFinal />
+        <SectionWrapper sectionId="listado" totalVersions={2} labels={["V1 Original", "V2 Nuevo"]}>
+          <ListadoSection />
+        </SectionWrapper>
+      </div>
+    </VersionProvider>
   );
 }
 

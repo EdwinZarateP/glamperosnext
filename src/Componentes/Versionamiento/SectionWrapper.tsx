07741d6fd857df281801d761c4da0b71d7bc17ea
Luis Velasquez <luise.velasquezg@gmail.com>
2026-04-02 01:05:25 -0500

    refactor: :recycle: refactoriza componente

--- src/Componentes/Versionamiento/SectionWrapper.tsx
+++ src/Componentes/Versionamiento/SectionWrapper.tsx
@@ -2,22 +2,12 @@ import VersionSwitcher from "./VersionSwitcher";
 
 interface Props {
   sectionId: string;
-  // totalVersions: number;  // ← recibe el total explícitamente
   children: React.ReactNode;
   labels?: string[];
 }
 
-// export default function SectionWrapper({ sectionId, totalVersions, children, labels }: Props) {
 export default function SectionWrapper({ sectionId, children, labels }: Props) {
   return (
-    // <div style={{ position: "relative" }}>
-    //   <VersionSwitcher
-    //     sectionId={sectionId}
-    //     totalVersions={totalVersions}
-    //     labels={labels}
-    //   />
-    //   {children}
-    // </div>
     <div style={{ position: "relative", background: "var(--crema)" }}>
       <VersionSwitcher
         sectionId={sectionId}

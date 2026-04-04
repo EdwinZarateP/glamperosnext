a3ddbbfff7dc13d5b5cd2c20404be817365cf362
Luis Velasquez <luise.velasquezg@gmail.com>
2026-04-04 11:41:12 -0500

    refactor: :recycle: organiza listado de versiones de ListadoGlampings
    .

--- src/app/prueba/page.tsx
+++ src/app/prueba/page.tsx
@@ -2,14 +2,15 @@
 
 import { VersionProvider, useVersion } from "@/context/VersionContext";
 import SectionWrapper from "@/Componentes/Versionamiento/SectionWrapper";
-import ListadoGlampings from "@/Componentes/ListadoGlampingsPrueba/ListadoGlampings";
-import ListadoGlampingsV2 from "@/Componentes/ListadoGlampingsPrueba/ListadoGlampingsV2";
+import ListadoGlampingsV1 from "@/Componentes/ListadoGlampingsPrueba/V1/ListadoGlampingsV1";
+import ListadoGlampingsV2 from "@/Componentes/ListadoGlampingsPrueba/V2/ListadoGlampingsV2";
+import ListadoGlampingsV3 from "@/Componentes/ListadoGlampingsPrueba/V3/ListadoGlampingsV3";
 import HeaderFinal from "../../Componentes/HeaderModificado";
 import "./estilos.css";
 
 function ListadoGlampingsSection() {
   const { current } = useVersion("listado-glamings");
-  const versiones = [<ListadoGlampings key="v1" />, <ListadoGlampingsV2 key="v2" />, <ListadoGlampingsV2 key="v3" />];
+  const versiones = [<ListadoGlampingsV1 key="v1" />, <ListadoGlampingsV2 key="v2" />, <ListadoGlampingsV3 key="v3" />];
   return versiones[current];
 }
 
@@ -18,7 +19,7 @@ function Prueba() {
     <VersionProvider>
       <div className="prueba-contenedor">
         <HeaderFinal />
-        <SectionWrapper sectionId="listado-glamings" labels={["V1 Original", "V2 Nuevo", "V2 Repetido"]}>
+        <SectionWrapper sectionId="listado-glamings" labels={["Versión 1", "Versión 2", "Versión 3"]}>
           <ListadoGlampingsSection />
         </SectionWrapper>
       </div>
